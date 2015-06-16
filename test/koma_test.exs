191c6e9f6b41f5463a35249d845b67fc8e661d09
Juho Hautala <juho.hautala@helsinki.fi>
2015-06-16 22:31:37 +0300

    uncomment tests + remove one case

--- test/koma_test.exs
+++ test/koma_test.exs
@@ -15,17 +15,13 @@ defmodule KomaTest do
   end
 
   # .reduce
-  test ".reduce/2 returns nil for []" do
-    # assert Koma.reduce([], fn (elem, acc) -> elem * acc end) == nil
-  end
-
   test ".reduce/2 returns element for singleton" do
-    # assert Koma.reduce([7], fn (elem, acc) -> elem + acc end) == 7
-    # assert Enum.reduce([7], fn (elem, acc) -> elem + acc end) == 7
-    # assert Koma.reduce([7], fn (elem, acc) -> elem * acc end) == 7
-    # assert Enum.reduce([7], fn (elem, acc) -> elem * acc end) == 7
-    # assert Koma.reduce([7], fn (elem, acc) -> elem / acc end) == 7
-    # assert Enum.reduce([7], fn (elem, acc) -> elem / acc end) == 7
+    assert Koma.reduce([7], fn (elem, acc) -> elem + acc end) == 7
+    assert Enum.reduce([7], fn (elem, acc) -> elem + acc end) == 7
+    assert Koma.reduce([7], fn (elem, acc) -> elem * acc end) == 7
+    assert Enum.reduce([7], fn (elem, acc) -> elem * acc end) == 7
+    assert Koma.reduce([7], fn (elem, acc) -> elem / acc end) == 7
+    assert Enum.reduce([7], fn (elem, acc) -> elem / acc end) == 7
   end
 
   test ".reduce/3 returns accumulator for []" do
